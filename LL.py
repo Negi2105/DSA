c441067df3e1240d7ef733c95547d089568a7de3
Sudershan Negi <sudershan.negi@plaksha.edu.in>
2024-02-01 13:57:01 +0530

    New

--- LL.py
+++ LL.py
@@ -137,16 +137,12 @@ class LinkedList():
                 result += ' -> '
             temp_node = temp_node.next
         return result
-<<<<<<< HEAD
     
 new=LinkedList()
 new.append(10)
 new.append(20)
 new.append(30)
-https://github.com/Negi2105/DSA
 print(new) 
-=======
->>>>>>> 8617936fc80f5e17966ab08c7f0ba1cb2a128acf
 
 new_linkedlist= LinkedList()
 #print(new_linkedlist.pop_first())
